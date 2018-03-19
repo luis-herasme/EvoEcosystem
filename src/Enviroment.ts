1ae65f82e335e871ccfac0f3760e9e7eb70be334
LuisHerasme <luis.alberto.herasme.cuevas@gmail.com>
2018-03-18 21:10:46 -0400

    Separation of entities

--- src/Enviroment.ts
+++ src/Enviroment.ts
@@ -3,7 +3,10 @@ import Entity from "./Entity"
 export default class Enviroment {
   public x: number
   public y: number
-  public Entities: Array<Entity>
+
+  public trees        : Array<Entity>
+  public vegetarians  : Array<Entity>
+  public carnivorous  : Array<Entity>
 
   constructor (x, y) {
     this.x = x
@@ -11,11 +14,23 @@ export default class Enviroment {
   }
 
   add (child :Entity) {
-    this.Entities.push(child)
+    if (child.tree) {
+      this.trees.push(child)
+    } else if (child.carnivore) {
+      this.carnivorous.push(child)
+    } else {
+      this.vegetarians.push(child)
+    }
   }
 
   remove (child :Entity) {
-    this.Entities.splice(this.Entities.indexOf(child), 1)
+    if (child.tree) {
+      this.trees.splice(this.trees.indexOf(child), 1)
+    } else if (child.carnivore) {
+      this.carnivorous.splice(this.carnivorous.indexOf(child), 1)
+    } else {
+      this.vegetarians.splice(this.vegetarians.indexOf(child), 1)
+    }
   }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ function see () {
   this.enviroment.Entities.forEach(element => {
     let near = this.findNearest(this.enviroment.Plants)
 
-    let nearPeligro = this.findNearest(this.enviroment.Carnivoros) 
+    let nearPeligro = this.findNearest(this.enviroment.carnivoros) 
      
     if (nearPeligro.distance < this.fieldOfVision) {
       this.position.moveTowards(near.entity, -1 * this.speed)  
@@ -30,12 +30,15 @@ function see () {
         this.eat(near.entity)
       }
     } else {
+
       if (near.distance < this.fieldOfVision) {
         this.position.moveTowards(near.entity, this.speed)
-      }
-    
-      if (near.distance < this.size + near.entity.size) {
-        this.eat(near.entity)
+
+        if (near.distance < this.size + near.entity.size) {
+          this.eat(near.entity)
+        }
+      } else {
+        this.moveRandom()
       }
     }
 
